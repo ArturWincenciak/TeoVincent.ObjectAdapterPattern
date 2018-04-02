1801107872be6a160084fdfa70d3b3ddd6913ce0
ArturWincenciak <teo.vincent.com@gmail.com>
2018-04-03 00:58:13 +0200

    add call request method in target test (RED)

---
+++ TeoVincent.ObjectAdapterPattern/AdapterTester.cs
@@ -0,0 +1,28 @@
+﻿using FakeItEasy;
+using Xunit;
+
+namespace TeoVincent.ObjectAdapterPattern
+{
+    public class AdapterTester
+    {
+        private readonly Client client;
+ 
+        public AdapterTester()
+        {
+            client = new Client();
+        }
+ 
+        [Fact]
+        public void call_request_method_in_target()
+        {
+            // arrange
+            ITarget target = A.Fake<ITarget>();
+ 
+            // act
+            client.CallRequest(target);
+ 
+            // assert
+            A.CallTo(() => target.Request()).MustHaveHappened();
+        }
+    }
+}
